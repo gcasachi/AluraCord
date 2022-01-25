dafe5b9a7a55447f845b499b6aea1bcf2c71d1f5
gcasachi <gcasachi@gmail.com>
2022-01-25 01:56:17 -0300

    Editing BackGround

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ function Titulo(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    const username = 'gcasachi';
   
     return (
       <>
@@ -67,8 +67,8 @@ export default function PaginaInicial() {
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
             backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+            backgroundImage: 'url(https://wallpapercave.com/wp/6FZlzVC.jpg)',
+            backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', backgroundBlendMode: 'multiply',
             
           }}
         >
@@ -82,7 +82,7 @@ export default function PaginaInicial() {
                 sm: 'row',
               },
               width: '100%', maxWidth: '700px',
-              borderRadius: '5px', padding: '32px', margin: '16px',
+              borderRadius: '10px', padding: '32px', margin: '16px',
               boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
               backgroundColor: appConfig.theme.colors.neutrals[700],
               opacity: '90%',
@@ -107,7 +107,7 @@ export default function PaginaInicial() {
                   neutral: {
                     textColor: appConfig.theme.colors.neutrals[200],
                     mainColor: appConfig.theme.colors.neutrals[900],
-                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                    mainColorHighlight: appConfig.theme.colors.primary[900],
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
                 }}
@@ -118,8 +118,8 @@ export default function PaginaInicial() {
                 fullWidth
                 buttonColors={{
                   contrastColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[500],
-                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColor: appConfig.theme.colors.primary[1000],
+                  mainColorLight: appConfig.theme.colors.primary[300],
                   mainColorStrong: appConfig.theme.colors.primary[600],
                 }}
               />
